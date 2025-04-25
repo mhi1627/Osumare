b3e08160bb2aa53f4625f4780e098a0731fc0792
mhi1627 <pvg1627@gmail.com>
2025-04-25 14:48:00 +0530

    minor fixes

--- src/Sections/Navbar/NavBar.jsx
+++ src/Sections/Navbar/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Button from '../../Components/Button/Button'
+import logo from "M:/React/Osumare/src/assets/osumare_logo.png"
 export default function NavBar() {
   return (
     <div className='w-full h-30 flex justify-between items-center px-2 sm:px-10'>
-     <img src="src\assets\osumare_logo.png" alt="" className='w-27 sm:w-40'/>
+     <img src={logo} alt="" className='w-27 sm:w-40'/>
      <Button style={`w-25 h-8 sm:w-52 sm:h-10 sm:font-bold text-sm border-1  hover:border-1-black `}>Contact Us</Button>
     </div>
   )
